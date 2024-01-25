3003a5245559945a01afed25dd3c4d4dccee4554
Alokhas48 <154874661+Alokhas48@users.noreply.github.com>
2024-01-25 07:52:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,13 +4,13 @@ import os
 
 from plugins.cb_data import app as Client2
 
-TOKEN = os.environ.get("TOKEN", "")
+TOKEN = os.environ.get("TOKEN", "6970285411:AAHHw0mq4Yowuv7U2sgxfmUsCrV3kERNJsc")
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "28647200"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "42e71744fb1829f43010bd6003224daf")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQG1HyAAJyskqcGO3R0Uqrz-UTpYZ_f9lCcSJBtBJDk-Pnk9OMmgmjTIfPifIHn5VPeJZ1kNFsgeTmaFheJVihOjt7pvlQMxjEE2FdgpJbi_E9XiWZou1ugGwO0l_CRlKJ2FDQQjmmMV9WkvWDxubUhWhXnzWeabuuN9kfMJ9ix7nL3xkHkP98UircuPFK0c4Tw2whruv6C6uA_NofxK7vpC0iIdg70QZvDfARQ70mmR2xmc-34VQG8Umm3zGzy6zTYEqMmN6B2D00h2zXaIMsEiADrE7xqskks-lbGSQSSiAPx5_xOGdV7YERr-zXqML83oKNm5hDvGagnAqa3e_nx-PIpfnwAAAAB2ODvWAA")
 
 
 bot = Client(
